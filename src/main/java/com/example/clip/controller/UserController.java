1e34be96f5711d8450260df77a2e8950f0020b70
fjzom <f.oviedo.m@hotmail.com>
2022-08-31 17:44:18 -0500

    first commit

---
+++ src/main/java/com/example/clip/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.example.clip.controller;
+
+public class UserController {
+}
---
+++ src/main/java/com/example/clip/model/User.java
@@ -0,0 +1,21 @@
+package com.example.clip.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+}
