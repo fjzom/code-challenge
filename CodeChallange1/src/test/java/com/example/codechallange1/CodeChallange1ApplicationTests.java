4bd733d8eff6dba4c1e402c0f0f78dbe7e400541
Fjzomoo7! <felipeoviedomaltez@gmail.com>
2023-02-21 17:24:04 -0600

    commit1

---
+++ CodeChallange1/src/test/java/com/example/codechallange1/CodeChallange1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codechallange1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallange1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
